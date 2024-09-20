967cc6b7a4e4d48d345579d619e81df21b3721fe
Angel Garcia <angelgarmar01@gmail.com>
2024-09-20 14:00:14 -0600

    Se agregan tests

--- src/test/java/com/transporte/controllers/ClienteControllerTest.java
+++ src/test/java/com/transporte/controllers/ClienteControllerTest.java
@@ -1,6 +1,9 @@
 package com.transporte.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.transporte.constants.Constants;
 import com.transporte.dto.RegisterClientRequest;
 import com.transporte.dto.RegisterClientResponse;
 import com.transporte.models.ResponseService;
@@ -49,6 +52,47 @@ class ClienteControllerTest {
         ResponseService clienteCreado = respuesta.getBody();
         assertNotNull(clienteCreado);
         log.info(clienteCreado.toString());
+    }
+
+    @Test
+    @Order(2)
+    void testGetClients() {
+        ResponseEntity<ResponseService> respuesta = client.getForEntity(crearUri(""), ResponseService.class);
+        assertEquals(HttpStatus.OK, respuesta.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, respuesta.getHeaders().getContentType());
+        assertNotNull(respuesta.getBody());
+        ResponseService response = respuesta.getBody();
+        assertNotNull(response.getData());
+    }
+
+    @Test
+    @Order(3)
+    void testGetClientById() throws JsonProcessingException {
+        ResponseEntity<ResponseService> respuesta = client.getForEntity(crearUri("/4"), ResponseService.class);
+        assertEquals(HttpStatus.OK, respuesta.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, respuesta.getHeaders().getContentType());
+        ResponseService response = respuesta.getBody();
+        assertNotNull(response);
+        log.info(response.toString());
+        assertNotNull(response.getData());
+        JsonNode json = objectMapper.readTree(objectMapper.writeValueAsString(response));
+        assertEquals("Cliente1", json.path("data").path("nombre").asText());
+        assertEquals("Apaterno1", json.path("data").path("apaterno").asText());
+        assertEquals("5523411690", json.path("data").path("celular").asText());
+    }
+
+    @Test
+    @Order(4)
+    void testDeleteClient() throws JsonProcessingException {
+        client.delete(crearUri("/4"));
+        ResponseEntity<ResponseService> respuesta = client.getForEntity(crearUri("/4"), ResponseService.class);
+        assertEquals(HttpStatus.OK, respuesta.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, respuesta.getHeaders().getContentType());
+        ResponseService response = respuesta.getBody();
+        assertNotNull(response);
+        JsonNode json = objectMapper.readTree(objectMapper.writeValueAsString(response));
+        assertEquals(Constants.RESPONSE_TYPE_ERROR, json.path("responseType").asText());
+        assertEquals(Constants.CLIENTE_NO_EXISTENTE, json.path("message").asText());
     }
 
     private String crearUri(String uri) {
